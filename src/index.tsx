cdf1589ae952a4e235196a9283e47178c7dba4f4
shrid1989 <shrikant.d60@gmail.com>
2021-08-20 00:24:57 +0100

    fixed ie11 promise issue and removed unwanted code

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { polyfill } from 'es6-promise'; polyfill();
 
 import SearchComponent from './components/searchComponent/searchComponent';
 import '../scss/app.scss';
